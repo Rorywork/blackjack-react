552151937428a7ddb41bfef42c79565e5244ad6f
Rorywork <rorywork95@outlook.com>
2020-02-20 15:17:47 +0000

    Approach 1

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 import GameBoard from "./GameBoard";
 
 class App extends React.Component {
-  state = { cards: [], playerCount: 0 };
+  state = { cards: [], playerCount: 0, dealerCount: 0 };
 
   componentDidMount = async () => {
     const response = await Axios.get(
@@ -13,11 +13,11 @@ class App extends React.Component {
     this.setState({ cards: response.data.cards });
   };
 
-  renderGameState = playerCount => {
+  renderGameState = (playerCount, dealerCount) => {
     console.log("renderGameState working");
 
     if (playerCount === 0) {
-      this.setState({ playerCount: 1 });
+      this.setState({ playerCount: 1, dealerCount: 1 });
     }
     if (playerCount === 1) {
       this.setState({ playerCount: 2 });
@@ -26,19 +26,16 @@ class App extends React.Component {
       this.setState({ playerCount: 3 });
     }
     if (playerCount === 3) {
-      this.setState({ playerCount: 4 });
+      this.setState({ dealerCount: 2 });
     }
-    if (playerCount === 4) {
-      this.setState({ playerCount: 5 });
+    if (playerCount === 3 && dealerCount === 2) {
+      this.setState({ dealerCount: 3 });
     }
-    if (playerCount === 5) {
-      this.setState({ playerCount: 6 });
+    if (playerCount === 3 && dealerCount === 3) {
+      this.setState({ dealerCount: 4 });
     }
-    if (playerCount === 6) {
-      this.setState({ playerCount: 7 });
-    }
-    if (playerCount === 7) {
-      this.setState({ playerCount: 8 });
+    if (playerCount === 3 && dealerCount === 4) {
+      this.setState({ dealerCount: 5 });
     } else {
       console.log("There is an error with the count!");
     }
@@ -48,16 +45,17 @@ class App extends React.Component {
     return (
       <div>
         Found: {this.state.cards.length} cards
-        <button onClick={e => this.setState({ count: 4 })}>Next Stage</button>
-        <h1> Phase {this.state.phase}</h1>
-        <h1> Count {this.state.count}</h1>
+        <button onClick={e => this.setState({ playerCount: 4 })}>
+          Next Stage
+        </button>
+        <h1> Count {this.state.playerCount}</h1>
         <button>Twist</button>
         <button>Stick</button>
         <GameBoard
           cards={this.state.cards}
           gameState={this.renderGameState}
-          count={this.state.count}
-          phase={this.state.phase}
+          playerCount={this.state.playerCount}
+          dealerCount={this.state.dealerCount}
         />
       </div>
     );
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -2,72 +2,72 @@ import "./Gameboard.css";
 import React from "react";
 import Card from "./Card.js";
 
-const Gameboard = ({ cards, count, gameState }) => {
+const Gameboard = ({ cards, playerCount, gameState }) => {
   const renderedList = cards.map(card => {
     return <Card key={card.code} card={card} />;
   });
 
-  if (count === 0) {
+  if (playerCount === 0) {
     return (
       <div className="gameboard">
         <div className="card-list player">{renderedList.slice(0, 2)}</div>
-        <button onClick={() => gameState(count)}>Test RenderState</button>
+        <button onClick={() => gameState(playerCount)}>Test RenderState</button>
       </div>
     );
   }
-  if (count === 1) {
+  if (playerCount === 1) {
     return (
       <div className="gameboard">
         <div className="card-list player">{renderedList.slice(0, 3)}</div>
-        <button onClick={() => gameState(count)}>Test RenderState</button>
+        <button onClick={() => gameState(playerCount)}>Test RenderState</button>
       </div>
     );
   }
-  if (count === 2) {
+  if (playerCount === 2) {
     return (
       <div className="gameboard">
         <div className="card-list player">{renderedList.slice(0, 4)}</div>
-        <button onClick={() => gameState(count)}>Test RenderState</button>
+        <button onClick={() => gameState(playerCount)}>Test RenderState</button>
       </div>
     );
   }
-  if (count === 3) {
+  if (playerCount === 3) {
     return (
       <div className="gameboard">
         <div className="card-list player">{renderedList.slice(0, 5)}</div>
-        <button onClick={() => gameState(count)}>Test RenderState</button>
+        <button onClick={() => gameState(playerCount)}>Test RenderState</button>
       </div>
     );
   }
-  if (count === 4) {
+  if (playerCount === 4) {
     return (
       <div className="gameboard">
         <div className="card-list dealer">{renderedList.slice(0, 7)}</div>
-        <button onClick={() => gameState(count)}>Test RenderState</button>
+        <button onClick={() => gameState(playerCount)}>Test RenderState</button>
       </div>
     );
   }
-  if (count === 5) {
+  if (playerCount === 5) {
     return (
       <div className="gameboard">
         <div className="card-list dealer">{renderedList.slice(0, 8)}</div>
-        <button onClick={() => gameState(count)}>Test RenderState</button>
+        <button onClick={() => gameState(playerCount)}>Test RenderState</button>
       </div>
     );
   }
-  if (count === 6) {
+  if (playerCount === 6) {
     return (
       <div className="gameboard">
         <div className="card-list dealer">{renderedList.slice(0, 9)}</div>
-        <button onClick={() => gameState(count)}>Test RenderState</button>
+        <button onClick={() => gameState(playerCount)}>Test RenderState</button>
       </div>
     );
   }
-  if (count === 7) {
+  if (playerCount === 7) {
     return (
       <div className="gameboard">
         <div className="card-list dealer">{renderedList.slice(0, 10)}</div>
-        <button onClick={() => gameState(count)}>Test RenderState</button>
+        <button onClick={() => gameState(playerCount)}>Test RenderState</button>
       </div>
     );
   }
